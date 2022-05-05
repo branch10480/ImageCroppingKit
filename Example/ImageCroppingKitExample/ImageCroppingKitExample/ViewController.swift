38fa9b8a9aab9c8b48d431a4e106d9db62684b26
branch10480 <toshiharu.imaeda@gmail.com>
2022-05-05 13:59:58 +0900

    Update

---
+++ Example/ImageCroppingKitExample/ImageCroppingKitExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ImageCroppingKitExample
+//
+//  Created by Toshiharu Imaeda on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var button: UIButton!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func didTapButton(_ sender: Any) {
+  }
+}
+
---
+++ Sources/ImageCroppingKit/View/ImageCroppingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCroppingViewController.swift
+//  
+//
+//  Created by Toshiharu Imaeda on 2022/05/05.
+//
+
+import UIKit
+
+class ImageCroppingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
